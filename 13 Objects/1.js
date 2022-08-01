384c51728f339e511d36923e815d70f5bf84675a
github_vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-08-01 14:23:00 +0300

    13-first_task_finished

---
+++ 13 Objects/1.js
@@ -0,0 +1,11 @@
+let user = {
+    name: "admin",
+    email: "admin@example.com",
+};
+
+console.log(user.name);
+console.log(user.email);
+
+user.email = "admin@itvdn.com";
+
+console.log(user.email);
---
+++ 13 Objects/4.js
@@ -0,0 +1,29 @@
+let user = {
+    balance: '$1,250.89',
+    age: 24,
+    name: {
+        first: 'Golden',
+        last: 'Clements'
+    },
+    company: 'EWAVES',
+    email: 'golden.clements@ewaves.io',
+    friends: [
+        {
+            id: 0,
+            name: 'Buchanan Austin'
+        },
+        {
+            id: 1,
+            name: 'Richmond Garrison'
+        },
+        {
+            id: 2,
+            name: 'Burns Cook'
+        },
+        {
+            id: 3,
+            name: 'Sally Mcpherson'
+        }
+    ],
+    favoriteFruit: 'strawberry'
+}
