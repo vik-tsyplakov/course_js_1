15379db4c3b745d026e9c84ba1727b48cd76bca0
github_vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-08-03 13:27:48 +0300

    14_fourth_task_finished

--- 14 Objects/4.js
+++ 14 Objects/4.js
@@ -1,17 +1,22 @@
-let obj1 = {
-    firstName: "Ivan",
-    lastName: "Ivanov",
-};
 
-let obj2 = {
-    firstName: "Jhon",
-    lastName: "Doe",
-};
-
-function print(obj) {
-    console.log("First Name: " + obj.firstName);
-    console.log("Last Name: " + obj.lastName);
+let initObject = function(firstName, lastName) {
+    return {
+        firstName,
+        lastName,
+        print() {
+            console.log("First Name: " + this.firstName);
+            console.log("Last Name: " + this.lastName);
+        }
+    }
 }
 
-print(obj1);
-print(obj2);
+let obj1 = initObject("Ivan", "Ivanov");
+let obj2 = initObject("Jhon", "Doe");
+let obj3 = initObject("Mary", "Smith");
+let obj4 = initObject("Nicola", "Tesla");
+
+ 
+obj1.print();
+obj2.print();
+obj3.print();
+obj4.print();
